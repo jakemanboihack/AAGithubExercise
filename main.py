4971cc79a95c9fb8b99579e2ab31d02b5544f807
Evan Weinberg <eweinberg@ssis.edu.vn>
2020-02-24 13:25:40 +0000

    Added structures for everyone to add their functions.

---
+++ main.py
@@ -0,0 +1,63 @@
+print("This is a demonstration of how Github works.\n")
+print("Your task is to take this file, add a function in the space below that returns your name and a message, and then call it in the function labeled ourMessages below.\n")
+
+
+# Hina's function goes here:
+
+
+
+#*************************************#
+
+# Leo's function goes here:
+
+
+
+#*************************************#
+
+# Nam's function goes here:
+
+
+
+#*************************************#
+
+# Son's function goes here:
+
+
+
+#*************************************#
+
+# Jaden's function goes here:
+
+
+
+#*************************************#
+
+# Jake's function goes here:
+
+
+
+#*************************************#
+
+# Tung's function goes here
+
+
+
+#*************************************#
+
+# Weinberg's function goes here:
+
+
+def weinbergMessage():
+  return "Weinberg is in charge around here. He should probably sweep the floor."
+
+
+#*************************************#
+
+def ourMessages():
+
+  # Weinberg's function is called and printed here:
+  print(weinbergMessage())
+
+
+#This will call the ourMessages function.
+ourMessages()
